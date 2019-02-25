3989791e01d9c878258ac6e5511782c3d118b9df
Brian Simonton <bsimonton78@gmail.com>
2019-02-25 20:32:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,16 @@
 
 def key_for_min_value(name_hash)
 
-name_hash.each do |item, price|
-  
+
+  smallest_key = nil
+  tiniest_value = nil
+  hash.each do |key, value|
+    if tiniest_value == nil || value < tiniest_value
+      tiniest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
 end
 
 end
